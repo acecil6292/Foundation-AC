52d681841c27d8801f11f3ef4cd71c7c085e0b7a
Aaron Cecil <acecil6292@gmail.com>
2018-08-22 21:16:18 -0500

    initial project

---
+++ WEEK1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WEEK_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //this is a comment
+
+            /*this
+            is 
+            a
+            comment */
+        } 
+    }
+}
